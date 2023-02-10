864d5fa0fcc1afcf582dbef17a7912f528837aa0
Anton Lugom <mogul086@mail.ru>
2023-02-10 19:40:45 +0300

    Класс для демонстрации работы с потоками

---
+++ Example/Class1.cs
@@ -0,0 +1,77 @@
+﻿namespace Example
+{
+    public class ThreadWorkClass
+    {
+        private static object o = new object();
+
+        public ThreadWorkClass()
+        {
+        }
+
+        public void WritePlus()
+        {
+            Console.WriteLine($"-->Запущен поток №: {Thread.CurrentThread.ManagedThreadId}");
+
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write("+");
+                Thread.Sleep(25);
+            }
+
+            Console.WriteLine($"<--Завершен поток поток №: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public void WriteMinus()
+        {
+            Console.WriteLine($"-->Запущен поток №: {Thread.CurrentThread.ManagedThreadId}");
+
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("-");
+                Thread.Sleep(50);
+            }
+
+            Console.WriteLine($"<--Завершен поток поток №: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+
+        public void WritePlusWithLock()
+        {
+            Console.WriteLine($"-->Запущен поток №: {Thread.CurrentThread.ManagedThreadId}");
+
+            lock(o)
+            {
+                Console.WriteLine("---Работа с критической секцией, блокировка доступа");
+                for (int i = 0; i < 40; i++)
+                {
+                    Console.Write("+");
+                    Thread.Sleep(25);
+                }
+
+            }
+            Console.WriteLine("---Блокировка снята");
+
+            Console.WriteLine($"<--Завершен поток поток №: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public void WriteMinusWithLock()
+        {
+            Console.WriteLine($"-->Запущен поток №: {Thread.CurrentThread.ManagedThreadId}");
+
+            lock (o)
+            {
+                Console.WriteLine("---Работа с критической секцией, блокировка доступа");
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.Write("-");
+                    Thread.Sleep(50);
+                }
+
+            }
+            Console.WriteLine("---Блокировка снята");
+
+            Console.WriteLine($"<--Завершен поток поток №: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+    }
+}
